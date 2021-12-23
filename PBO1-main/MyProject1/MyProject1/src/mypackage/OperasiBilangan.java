5010bcabd5e4720ef497bef63bac3c5ee2c49be1
yahyahamid <71340120+yahyahamid@users.noreply.github.com>
2021-12-23 14:36:05 +0700

    Add files via upload

---
+++ PBO1-main/MyProject1/MyProject1/src/mypackage/OperasiBilangan.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mypackage;
+
+/**
+ *
+ * @author hp
+ */
+public class OperasiBilangan {
+    public class OperasiBilangan {
+    
+    // atribut
+    public int bilPertama;
+    public int bilKedua;
+    
+    // methods
+    public void hitungPenjumlahan(){
+        int hasil = bilPertama + bilKedua;
+        System.out.println("Hasil penjumlahannya: " + hasil);
+    }
+    
+    public void hitungPengurangan(){
+        int hasil = bilPertama - bilKedua;
+        System.out.println("Hasil pengurangannya: " + hasil);
+    }
+    
+    public void hitungPerkalian(){
+        int hasil = bilPertama * bilKedua;
+        System.out.println("Hasil perkaliannya: " + hasil);
+    }
+    
+    public void hitungPembagian(){
+        double hasil = (double)bilPertama / (double)bilKedua;
+        System.out.println("Hasil pembagiannya: " + hasil);
+    }
+    
+}
+}
